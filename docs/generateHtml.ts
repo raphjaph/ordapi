521e5a8e1cee245653813d3c976f5b7ffc70d98b
raph <raphjaph@protonmail.com>
2025-02-07 16:43:11 +0100

    Release 0.0.5 (#23)

--- docs/generateHtml.ts
+++ docs/generateHtml.ts
@@ -4,7 +4,7 @@ import path from 'path';
 function generateHtml() {
   const config = {
     repoAddress: 'https://github.com/raphjaph/ordapi',
-    version: 'v0.0.4'
+    version: 'v0.0.5'
   };
   const scriptsContent = fs.readFileSync('docs/scripts.js', 'utf-8');
   const apiDocs = JSON.parse(fs.readFileSync('docs/api-docs.json', 'utf-8'));
